a35089f969c8e08db3883162d11bc73a46c9a10e
unknown <v.birembaut@J001-0133-07.etudiant.rubika-edu.com.local>
2015-11-23 13:50:04 +0100

    initialisation

---
+++ Assets/Scripts/Manager/MenuManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+    void Start()
+    {        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnClickPlay()
+    {
+        Application.LoadLevel(2);
+    }
+
+    public void BackNavigationButton() 
+    {
+        Application.LoadLevel(Application.loadedLevel - 1);
+    }
+}
---
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+    static SoundManager instance;
+    public static SoundManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+    void Awake()
+    {
+        instance = this;
+    }
+
+	private int currentSound = 0;
+
+	public GameObject currentZik;
+
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(this);
+		if(PlayerPrefs.GetInt ("sound")!= null)
+			currentSound = PlayerPrefs.GetInt ("sound");
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	public void playSound(AudioClip audio, float volume)
+	{
+		GameObject currentMusique = (GameObject)Instantiate (currentZik, Vector3.zero, Quaternion.identity);
+		AudioSource needZik =  currentMusique.AddComponent<AudioSource>();
+		needZik.clip = audio;
+		needZik.Play ();
+		if(currentSound == 0)
+			needZik.volume = volume;
+		else
+			needZik.volume = 0;
+	}
+
+}
